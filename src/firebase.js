be2317ed42c4909ed3ac33eb358de827396c30f7
mauryakhushiramjeet <mauryakhushiramjeet@gmail.com>
2025-07-24 10:52:17 +0530

    chnages in env file

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBgw4symSSqu3IEIu1dv21cgkswSSonP6E",
-  authDomain: "blog-app-1332c.firebaseapp.com",
-  projectId: "blog-app-1332c",
-  storageBucket: "blog-app-1332c.firebasestorage.app",
-  messagingSenderId: "526431247971",
-  appId: "1:526431247971:web:8c54b54e6ff3cd8278e256",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
